b0271c329fc1c36f31bf3dbe72824455a0d3b2fc
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-02-07 23:21:28 +0600

    prob 4

--- problem04.js
+++ problem04.js
@@ -1,7 +1,4 @@
 function password(obj) {
-  //write your code here
-  //console.log(Object.keys(obj).length);
-  // console.log(obj.birthYear.toString());
   if (Object.keys(obj).length === 3 && obj.birthYear.toString().length === 4) {
     let pass = `${obj.siteName}#${obj.name}@${obj.birthYear}`;
     let NewPass = pass[0].toLocaleUpperCase() + pass.slice(1);
@@ -12,9 +9,9 @@ function password(obj) {
 }
 
 const myObj = {
-  name: "yahya",
-  birthYear: 2202,
-  siteName: "google",
+  name: "rahat",
+  birthYear: 2002,
+  siteName: "Facebook",
 };
 
 const output = password(myObj);
