9024dbf1392fd287ea9bcc715f82d8db01ca6cf3
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-02-07 19:00:54 +0600

    problem1 solved

--- Problem01.js
+++ Problem01.js
@@ -1,3 +1,15 @@
 function calculateMoney(ticketSale) {
   // You have to write your code here
+  const perTicketPrice = 120;
+  let totalticketImcome = perTicketPrice * ticketSale;
+
+  let fixedCost = 500 + 8 * 50;
+  //console.log(fixedCost);
+
+  let netBalance = totalticketImcome - fixedCost;
+
+  return netBalance;
 }
+
+const mySale = calculateMoney(93);
+console.log(mySale);
